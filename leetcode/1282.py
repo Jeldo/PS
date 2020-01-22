37a62dce7da8929d2f2e181b409b5fa014fce812
Jeldo <astrokwak@gmail.com>
2020-01-22 15:07:30 +0900

    1282

--- leetcode/1282.py
+++ leetcode/1282.py
@@ -1,14 +1,22 @@
+'''
+Greedy
+Time Complexity: O(n)
+'''
+
+
 class Solution:
     def groupThePeople(self, groupSizes):
+        # 1<= len(groupSizes) <= 500
+        # 1<= groupSizes[i] <= len(groupSizes)
         answer = list()
         group_dict = dict()
-        for i in range(len(groupSizes)):
-            if groupSizes[i] not in group_dict.keys():
+        for i in range(len(groupSizes)):  # O(n)
+            if groupSizes[i] not in group_dict.keys():  # O(1)
                 group_dict[groupSizes[i]] = list()
-            group_dict[groupSizes[i]].append(i)
+            group_dict[groupSizes[i]].append(i)  # O(1)
             if len(group_dict[groupSizes[i]]) == groupSizes[i]:
                 answer.append(group_dict[groupSizes[i]])
-                del group_dict[groupSizes[i]]
+                del group_dict[groupSizes[i]]  # O(1)
         return answer
 
 

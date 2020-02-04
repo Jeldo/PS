af920a41a62c6aca420ebffa1392273904dd71ce
Jeldo <astrokwak@gmail.com>
2020-02-05 01:57:28 +0900

    1302

--- leetcode/1302.py
+++ leetcode/1302.py
@@ -11,31 +11,68 @@ class Solution:
         self.max_level = 0
         self.total = 0
 
-    def deepestLeavesSum(self, TreeNode, level):
+    # dumb
+    def deepestLeavesSum(self, TreeNode):
+        total = 0
+        q = list()
+        new_q = list()
+        q.append(TreeNode)
+        while q:
+            cur_node = q.pop(0)
+            if not cur_node.left and not cur_node.right and not new_q:
+                total += cur_node.val
+            if cur_node.left:
+                new_q.append(cur_node.left)
+                total = 0
+            if cur_node.right:
+                new_q.append(cur_node.right)
+                total = 0
+            if not q:
+                q = new_q.copy()
+                new_q.clear()
+        return total
 
-        if not TreeNode:
+    # BFS
+    def deepestLeavesSum2(self, root):
+        if not root:
             return 0
-        # print(TreeNode.val, level, self.max_level, self.total)
-        if level == self.max_level and not TreeNode.left and not TreeNode.right:
-            self.total += TreeNode.val
-        if level > self.max_level and not TreeNode.left and not TreeNode.right:
-            self.total = TreeNode.val
-            self.max_level = level
-        else:
-            self.deepestLeavesSum(TreeNode.left, level + 1)
-            self.deepestLeavesSum(TreeNode.right, level + 1)
-        return self.total
+        q = [root]
+        total = 0
+        while q:
+            for _ in range(len(q)):
+                node = q.pop(0)
+                total += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if q:
+                total = 0
+        return total
 
 
-treelist = [TreeNode(x) for x in range(1, 9)]
+# treelist = [TreeNode(x) for x in range(1,9)]
 
-treelist[0].left = treelist[1]      # 2
-treelist[0].right = treelist[2]     # 3
-treelist[1].left = treelist[3]      # 4
-treelist[1].right = treelist[4]     # 5
-treelist[3].left = treelist[6]      # 7
-treelist[2].right = treelist[5]     # 6
-treelist[5].right = treelist[7]     # 8
+# treelist[0].left = treelist[1]      # 2
+# treelist[0].right = treelist[2]     # 3
+# treelist[1].left = treelist[3]      # 4
+# treelist[1].right = treelist[4]     # 5
+# treelist[3].left = treelist[6]      # 7
+# treelist[2].right = treelist[5]     # 6
+# treelist[5].right = treelist[7]     # 8
 
-s = Solution().deepestLeavesSum(treelist[0], 0)
-print(s)
+nums = [38, 43, 70, 16, 78, 91, 71, 27, 71, 71]
+treelist = [TreeNode(x) for x in nums]
+
+treelist[0].left = treelist[1]
+treelist[0].right = treelist[2]
+treelist[1].left = treelist[3]
+treelist[2].left = treelist[4]
+treelist[2].right = treelist[5]
+treelist[3].right = treelist[6]
+treelist[4].left = treelist[7]
+treelist[5].left = treelist[8]
+treelist[7].right = treelist[9]
+
+s = Solution().deepestLeavesSum(treelist[0])
+print('answer:', s)

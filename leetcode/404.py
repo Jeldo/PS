1633aab55b1f5750269a3a25e5fa8e32fc408f64
Jeldo <astrokwak@gmail.com>
2020-02-04 15:51:17 +0900

    404

---
+++ leetcode/404.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.total = 0
+
+    # dumbass
+    def sumall(self, root):
+        if root is None:
+            return 0
+        if root.left is not None and root.left.left == None and root.left.right == None:
+            self.total += root.left.val
+        if root.left:
+            self.sumall(root.left)
+        if root.right:
+            self.sumall(root.right)
+
+    def sumOfLeftLeaves2(self, root):
+        self.sumall(root)
+        return self.total
+
+    # recursion
+    def sumOfLeftLeaves(self, root):
+        if root is None:
+            return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        else:
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)

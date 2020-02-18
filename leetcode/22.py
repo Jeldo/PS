05f5129db54638b47ffed2be49bab26b8d609ba5
Jeldo <astrokwak@gmail.com>
2020-02-18 15:16:46 +0900

    22

--- leetcode/22.py
+++ leetcode/22.py
@@ -2,22 +2,19 @@ class Solution:
     def generateParenthesis(self, n):
         parenthesis = list()
 
-        def dfs(cur_parentheses: str, next_p: str, stack: list):
-            cur_parentheses += next_p
-            print('{}\t\t\t\t{}'.format(cur_parentheses, stack))
+        def dfs(cur_parentheses: str, stack: list):
+            # print('{}\t\t\t\t{}'.format(cur_parentheses, stack))
             if len(cur_parentheses) == n*2 and len(stack) == 0:
                 parenthesis.append(cur_parentheses)
             else:
                 if cur_parentheses.count('(') < n:
                     stack.append('(')
-                    dfs(cur_parentheses, '(', stack)
+                    dfs(cur_parentheses + '(', stack)
                 if cur_parentheses.count(')') < n and cur_parentheses.count(')') < cur_parentheses.count('('):
                     if len(stack) > 0 and stack[-1] == '(':
                         stack.pop()
-                    dfs(cur_parentheses, ')', stack)
-        p = str()
-        stack = ['(']
-        dfs(p, '(', stack)
+                    dfs(cur_parentheses + ')', stack)
+        dfs('(', ['('])
         return parenthesis
 
 

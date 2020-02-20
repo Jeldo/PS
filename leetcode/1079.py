e235b78bcbc36ba1239941e18aa5334c333711b2
Jeldo <astrokwak@gmail.com>
2020-02-20 23:51:18 +0900

    1079

---
+++ leetcode/1079.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+
+class Solution:
+    # 1 backtracking
+    def numTilePossibilities(self, tiles: str):
+        result = set()
+
+        def dfs(path, tile):
+            print(path)
+            if path not in result:
+                if path:
+                    result.add(path)
+                for i in range(0, len(tile)):
+                    dfs(path+tile[i], tile[:i]+tile[i+1:])
+        dfs('', tiles)
+        return len(result)
+
+    # 2 brute force using permutation
+    def numTilePossibilities2(self, tiles: str):
+        tile_set = set()
+        for i in range(1, len(tiles)+1):
+            for p in permutations(tiles, i):
+                s = ''.join(p)
+                if s not in tile_set:
+                    tile_set.add(s)
+        return len(tile_set)
+
+
+s = Solution().numTilePossibilities("AABC")
+print(s)
--- leetcode/22.py
+++ leetcode/22.py
@@ -1,24 +1,38 @@
 class Solution:
+    # def generateParenthesis(self, n):
+    #     parenthesis = list()
+
+    #     def dfs(cur_parentheses: str, stack: list):
+    #         # print('{}\t\t\t\t{}'.format(cur_parentheses, stack))
+    #         if len(cur_parentheses) == n*2 and len(stack) == 0:
+    #             parenthesis.append(cur_parentheses)
+    #         else:
+    #             if cur_parentheses.count('(') < n:
+    #                 stack.append('(')
+    #                 dfs(cur_parentheses + '(', stack)
+    #             if cur_parentheses.count(')') < n and cur_parentheses.count(')') < cur_parentheses.count('('):
+    #                 if len(stack) > 0 and stack[-1] == '(':
+    #                     stack.pop()
+    #                 dfs(cur_parentheses + ')', stack)
+    #     dfs('(', ['('])
+    #     return parenthesis
+
     def generateParenthesis(self, n):
         parenthesis = list()
 
-        def dfs(cur_parentheses: str, stack: list):
-            # print('{}\t\t\t\t{}'.format(cur_parentheses, stack))
-            if len(cur_parentheses) == n*2 and len(stack) == 0:
-                parenthesis.append(cur_parentheses)
-            else:
-                if cur_parentheses.count('(') < n:
-                    stack.append('(')
-                    dfs(cur_parentheses + '(', stack)
-                if cur_parentheses.count(')') < n and cur_parentheses.count(')') < cur_parentheses.count('('):
-                    if len(stack) > 0 and stack[-1] == '(':
-                        stack.pop()
-                    dfs(cur_parentheses + ')', stack)
-        dfs('(', ['('])
+        def dfs(cur_parenthesis, open_p, close_p):
+            if len(cur_parenthesis) == n * 2:
+                parenthesis.append(cur_parenthesis)
+                return
+            if open_p < n:
+                dfs(cur_parenthesis+'(', open_p+1, close_p)
+            if close_p < open_p:
+                dfs(cur_parenthesis+')', open_p, close_p+1)
+        dfs('', 0, 0)
         return parenthesis
 
 
-n = 4
+n = 3
 
 s = Solution().generateParenthesis(n)
 print(s)

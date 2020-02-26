bd9b25113457783d0765ff7e440c64745453fdaa
Jeldo <astrokwak@gmail.com>
2020-02-27 02:35:33 +0900

    592

--- leetcode/592.py
+++ leetcode/592.py
@@ -1,8 +1,45 @@
 import math
+import re
+from fractions import Fraction
 
 
 class Solution:
+    # using re with fraction module
     def fractionAddition(self, expression: str):
+        numbers = re.compile('[-+]').split(expression)
+        total = sum([Fraction(x) for x in numbers if x])
+        return '{}/{}'.format(total.numerator, total.denominator)
+
+    # using re without fraction module
+    def fractionAddition2(self, expression: str):
+        numerator_list = list()
+        denominator_list = list()
+        numbers = re.compile('[-+]').split(expression)
+        numbers = [x for x in numbers if x]
+        for n in numbers:
+            fraction = n.split('/')
+            numerator_list.append(int(fraction[0]))
+            denominator_list.append(int(fraction[1]))
+
+        def lcm(x, y):
+            return x*y // math.gcd(x, y)
+
+        common_denominator = 1
+        for i in range(0, len(denominator_list)):
+            common_denominator = lcm(
+                common_denominator, denominator_list[i])
+
+        sum_of_denominators = 0
+        for i in range(len(numerator_list)):
+            multiply = common_denominator // denominator_list[i]
+            sum_of_denominators += numerator_list[i] * multiply
+
+        divisor = math.gcd(sum_of_denominators, common_denominator)
+        result = str(sum_of_denominators//divisor) + \
+            '/'+str(common_denominator//divisor)
+        return result
+
+    def fractionAddition3(self, expression: str):
         numerator_list = list()
         denominator_list = list()
         temp_string = str()
@@ -32,19 +69,13 @@ class Solution:
             multiply = common_denominator // denominator_list[i]
             numsum += numerator_list[i] * multiply
 
-        divide = math.gcd(numsum, common_denominator)
-        result = str(numsum//divide)+'/'+str(common_denominator//divide)
-
-        # print(numerator_list)
-        # print(denominator_list)
-        # print(common_denominator)
+        divisor = math.gcd(numsum, common_denominator)
+        result = str(numsum//divisor)+'/'+str(common_denominator//divisor)
 
         return result
 
 
-# fractions = ["-1/2+1/2", "-1/2+1/2+1/3", "1/3-1/2", "5/3+1/3"]
-# fractions = ["-1/2+1/2"]
-fractions = ["-5/2+10/3+7/9"]
+fractions = ["-1/2+1/2", "-1/2+1/2+1/3", "1/3-1/2", "5/3+1/3"]
 for f in fractions:
     s = Solution().fractionAddition(f)
     print(s)

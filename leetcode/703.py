db7b2aca0962d76dd915b664b7a23ab1b1ba53b2
Jeldo <astrokwak@gmail.com>
2020-02-28 19:36:34 +0900

    703

--- leetcode/703.py
+++ leetcode/703.py
@@ -1,27 +1,31 @@
+'''
+using min-heap
+'''
+import heapq
+
+
 class KthLargest:
 
+    # O(nlogn)
     def __init__(self, k, nums):
         self.k = k
-        self.nums = nums
+        self.heap_nums = sorted(nums, reverse=True)[:k]
+        heapq.heapify(self.heap_nums)
 
+    # O(logn)
     def add(self, val):
-        self.nums.append(val)
-        self.nums.sort(reverse=True)
-        return self.nums[2]
-
-        # Your KthLargest object will be instantiated and called as such:
-        # obj = KthLargest(k, nums)
-        # param_1 = obj.add(val)
-
-    def show(self):
-        print(self.nums)
+        if len(self.heap_nums) < self.k:
+            heapq.heappush(self.heap_nums, val)
+        elif val > self.heap_nums[0]:
+            heapq.heappushpop(self.heap_nums, val)
+        return self.heap_nums[0]
 
 
-k = 3
-nums = [4, 5, 8, 2]
+k = 2
+nums = [0]
 kl = KthLargest(k, nums)
+print(kl.add(-1))
+print(kl.add(1))
+print(kl.add(-2))
+print(kl.add(-4))
 print(kl.add(3))
-print(kl.add(5))
-print(kl.add(10))
-print(kl.add(9))
-print(kl.add(4))

c5cb4ddc12a63ebe595688c0ee1f0b1dd048a4fc
Jeldo <astrokwak@gmail.com>
2020-04-02 16:17:05 +0900

    856

--- leetcode/solved/856.py
+++ leetcode/solved/856.py
@@ -1,15 +1,32 @@
 class Solution:
     def scoreOfParentheses(self, S):
-        return
+        score = 0
+        depth = 0
+        prev = ''
+        cur = ''
+        for p in S:
+            cur = p
+            if p == '(':
+                depth += 1
+            else:
+                depth -= 1
+                if prev == '(' and cur == ')':
+                    if depth == 0:
+                        score += 1
+                    else:
+                        score += 2**(depth)
+            prev = p
+        return score
 
 
 cases = [
     '()',
     '(())',
     '()()',
-    '(()(()))'
+    '(()(()))',
+    '(()(()(())))'
 ]
 
 for c in cases:
-    s = Solution().queryString(c)
+    s = Solution().scoreOfParentheses(c)
     print(s)

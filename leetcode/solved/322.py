8eda5c052c2bcb088fcc06b3e298de8a52c042e7
Jeldo <astrokwak@gmail.com>
2020-04-29 14:58:09 +0900

    322

---
+++ leetcode/solved/322.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: list, amount: int):
+        c = [0 for _ in range(0, amount + 1)]
+        for i in range(1, amount + 1):
+            candidates = [c[i-coin]
+                          for coin in coins if 0 <= i - coin and 0 <= c[i-coin]]
+            if not candidates:
+                c[i] = -1
+            else:
+                c[i] = min(candidates) + 1
+        return c[amount] if 0 <= c[amount] else -1
+
+
+cases = [
+    [[1, 2, 5], 11],
+    [[2], 3],
+    [[1, 3, 5, 7], 23],
+    [[1], 0],
+    [[1, 5, 10, 21, 25], 63],
+    [[2], 4],
+    [[2, 3], 5],
+]
+
+for c in cases:
+    s = Solution().coinChange(c[0], c[1])
+    print(s)

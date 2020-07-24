dde6cb3d7dd9a635a0d78f89c83c4fe504e80305
Jeldo <astrokwak@gmail.com>
2020-07-24 17:47:56 +0900

    503

--- leetcode/pending review/1019.py
+++ leetcode/pending review/1019.py
@@ -1,72 +1,37 @@
 '''
-Category: Linked List
-Time Complexity: 
+Category: Stack, Linked List
 '''
 
 
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-    def __str__(self):
-        return str(self.val)
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
     def nextLargerNodes(self, head: ListNode):
+        answer = []
+        compare = head.val
+        cur = node = head
+        while cur.val >= node.val:
+            if cur.val < node.val:
+                compare = node.val
+            node = node.next
         return
 
-    # brute force, time limit exceeds
-    def nextLargerNodes2(self, head: ListNode):
-        def dfs(node: ListNode, origin: ListNode):
-            if not node.next:
-                ans.append(0)
-            else:
-                if origin.val < node.next.val:
-                    ans.append(node.next.val)
-                    return
-                dfs(node.next, origin)
-
-        ans = list()
-        if not head:
-            return [0]
-        else:
-            cur_node = head
-            while cur_node:
-                dfs(cur_node, cur_node)
-                cur_node = cur_node.next
-        return ans
-
-
-a = ListNode(2)
-b = ListNode(1)
-c = ListNode(5)
-a.next = b
-b.next = c
-
-aa = ListNode(1)
-bb = ListNode(7)
-cc = ListNode(5)
-dd = ListNode(1)
-ee = ListNode(9)
-ff = ListNode(2)
-gg = ListNode(5)
-hh = ListNode(1)
-
-aa.next = bb
-bb.next = cc
-cc.next = dd
-dd.next = ee
-ee.next = ff
-ff.next = gg
-gg.next = hh
 
 cases = [
-    a,
-    aa
+    [2, 1, 5],
+    [2, 7, 4, 3, 5],
+    [1, 7, 5, 1, 9, 2, 5, 1]
 ]
 
 for c in cases:
-    s = Solution().nextLargerNodes(c)
+    head = None
+    temp_next = None
+    for n in reversed(c):
+        head = ListNode(n, temp_next)
+        temp_next = head
+    s = Solution().nextLargerNodes(head)
     print(s)
--- leetcode/solved/503.py
+++ leetcode/solved/503.py
@@ -6,11 +6,20 @@ Time Complexity: O(n)
 
 class Solution:
     def nextGreaterElements(self, nums):
-        return []
+        stack, res = [], [-1] * len(nums)
+        for _ in range(2):
+            for i in range(len(nums)):
+                while stack and nums[stack[-1]] < nums[i]:
+                    res[stack.pop()] = nums[i]
+                    print(i, stack, res)
+                stack.append(i)
+                print(i, stack, res)
+        return res
 
 
 cases = [
-    [1, 2, 1]
+    [1, 2, 1],  # 2 -1 2
+    [3, 1, 2]   # -1 3 3
 ]
 
 for c in cases:

69ff96b56ace6add98fc804893845c3b1ab984f1
Jeldo <astrokwak@gmail.com>
2020-07-28 19:30:39 +0900

    22

--- leetcode/solved/22.py
+++ leetcode/solved/22.py
@@ -1,38 +1,29 @@
+'''
+Category: String
+'''
+
+
 class Solution:
-    # def generateParenthesis(self, n):
-    #     parenthesis = list()
+    def generateParenthesis(self, n: int):
+        answer = []
 
-    #     def dfs(cur_parentheses: str, stack: list):
-    #         # print('{}\t\t\t\t{}'.format(cur_parentheses, stack))
-    #         if len(cur_parentheses) == n*2 and len(stack) == 0:
-    #             parenthesis.append(cur_parentheses)
-    #         else:
-    #             if cur_parentheses.count('(') < n:
-    #                 stack.append('(')
-    #                 dfs(cur_parentheses + '(', stack)
-    #             if cur_parentheses.count(')') < n and cur_parentheses.count(')') < cur_parentheses.count('('):
-    #                 if len(stack) > 0 and stack[-1] == '(':
-    #                     stack.pop()
-    #                 dfs(cur_parentheses + ')', stack)
-    #     dfs('(', ['('])
-    #     return parenthesis
-
-    def generateParenthesis(self, n):
-        parenthesis = list()
-
-        def dfs(cur_parenthesis, open_p, close_p):
-            if len(cur_parenthesis) == n * 2:
-                parenthesis.append(cur_parenthesis)
+        def dfs(p, opened, closed):
+            nonlocal answer
+            if len(p) == n * 2:
+                answer.append(p)
                 return
-            if open_p < n:
-                dfs(cur_parenthesis+'(', open_p+1, close_p)
-            if close_p < open_p:
-                dfs(cur_parenthesis+')', open_p, close_p+1)
+            if opened < n:
+                dfs(p+'(', opened + 1, closed)
+            if opened > closed:
+                dfs(p+')', opened, closed + 1)
         dfs('', 0, 0)
-        return parenthesis
+        return answer
 
 
-n = 3
+cases = [
+    3, 4
+]
 
-s = Solution().generateParenthesis(n)
-print(s)
+for c in cases:
+    s = Solution().generateParenthesis(c)
+    print(s)

035c0434a7be3989d0a213343ef5c1512f78d536
Jeldo <astrokwak@gmail.com>
2020-08-06 00:14:56 +0900

    1026

--- leetcode/solved/1026.py
+++ leetcode/solved/1026.py
@@ -6,10 +6,10 @@ Space Complexity: O(1)
 
 
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
@@ -19,62 +19,28 @@ class Solution:
         def dfs(node: TreeNode, max_val, min_val):
             nonlocal max_diff
             if not node:
-                diff = max_val - min_val
-                max_diff = max(max_diff, diff)
                 return
             max_val = max(max_val, node.val)
             min_val = min(min_val, node.val)
+            max_diff = max(max_diff, abs(max_val - min_val))
             dfs(node.left, max_val, min_val)
             dfs(node.right, max_val, min_val)
-
         dfs(root, root.val, root.val)
         return max_diff
 
-    # too much time, too many memories consuming
-    def maxAncestorDiff2(self, root: TreeNode):
-        nodes = list()
-        max_diff = 0
 
-        def dfs(node: TreeNode, nodes: list):
-            nonlocal max_diff
-            if not node:
-                diff = max(nodes)-min(nodes)
-                max_diff = max(max_diff, diff)
-                return
-            new_nodes = nodes + [node.val]
-            dfs(node.left, new_nodes)
-            dfs(node.right, new_nodes)
-        dfs(root, nodes)
-        return max_diff
+cases = []
 
-
-cases = list()
-
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-head.left.left = TreeNode(4)
-
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-head.left.left = TreeNode(4)
-head.left.right = TreeNode(5)
-
-head = TreeNode(8)
-head.left = TreeNode(3)
-head.right = TreeNode(10)
-head.left.left = TreeNode(1)
-head.left.right = TreeNode(6)
-head.left.right.left = TreeNode(4)
-head.left.right.right = TreeNode(7)
-head.right.right = TreeNode(14)
-head.right.right.left = TreeNode(13)
-cases.append(head)
+root = TreeNode(8)
+root.left = TreeNode(3)
+root.right = TreeNode(10)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(6)
+root.right.right = TreeNode(14)
+root.left.right.left = TreeNode(4)
+root.left.right.right = TreeNode(7)
+root.right.right.left = TreeNode(13)
+cases.append(root)
 
 for c in cases:
     s = Solution().maxAncestorDiff(c)

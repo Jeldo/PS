f2c13b4e4dd86efd92b537a3da5e1c8f61d13812
Jeldo <astrokwak@gmail.com>
2020-08-06 18:02:26 +0900

    515

--- leetcode/solved/515.py
+++ leetcode/solved/515.py
@@ -1,37 +1,45 @@
 '''
-Category: BFS, Tree, DFS
+Category: DFS, Tree
+Time Complexity: O(n)
 '''
 
-from queue import deque
+
+from collections import defaultdict
 
 
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-def showNodes(head: TreeNode):
-    def dfs(head: TreeNode):
-        if head:
-            print(head.val, end=' ')
-            dfs(head.left)
-            dfs(head.right)
-    dfs(head)
-    print()
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
     def largestValues(self, root: TreeNode):
+        d = dict()
+
+        def dfs(node: TreeNode, level):
+            if not node:
+                return
+            if not level in d:
+                d[level] = node.val
+            else:
+                d[level] = max(d[level], node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+        dfs(root, 0)
+        return [v for v in d.values()]
+
+    # BFS
+    def largestValues2(self, root: TreeNode):
         if not root:
             return []
-        q = deque()
+        q = list()
         q.append(root)
         max_values = list()
 
         while q:
-            temp_q = deque()
+            temp_q = list()
             for node in q:
                 if node.left:
                     temp_q.append(node.left)
@@ -43,37 +51,33 @@ class Solution:
         return max_values
 
 
-cases = list()
+cases = []
 
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-cases.append(head)
+root = TreeNode(1)
+root.left = TreeNode(3)
+root.right = TreeNode(2)
+root.left.left = TreeNode(5)
+root.left.right = TreeNode(3)
+root.right.right = TreeNode(9)
+cases.append(root)
 
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-head.left.left = TreeNode(4)
-cases.append(head)
+root = TreeNode(0)
+root.left = TreeNode(-1)
+cases.append(root)
 
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-head.left.left = TreeNode(4)
-head.left.right = TreeNode(5)
-cases.append(head)
-
-head = TreeNode(1)
-head.left = TreeNode(2)
-head.right = TreeNode(3)
-head.left.left = TreeNode(4)
-head.left.right = TreeNode(5)
-head.right.left = TreeNode(8)
-head.right.right = TreeNode(7)
-cases.append(head)
-
-cases.append([])
+root = TreeNode(1)
+root.left = TreeNode(3)
+root.right = TreeNode(4)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(2)
+root.right.left = TreeNode(1)
+root.right.right = TreeNode(5)
+root.left.left.right = TreeNode(10)
+root.left.right.right = TreeNode(4)
+root.right.right.left = TreeNode(3)
+root.right.right.right = TreeNode(5)
+cases.append(root)
 
 for c in cases:
-    s = Solution().largestValues2(c)
+    s = Solution().largestValues(c)
     print(s)

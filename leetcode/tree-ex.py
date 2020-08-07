664ad5ac30f3683a6778da90fca0d134c76f4487
Jeldo <astrokwak@gmail.com>
2020-08-07 11:59:09 +0900

    tree_ex

---
+++ leetcode/tree-ex.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+cases = []
+
+root = TreeNode(1)
+# root.left = TreeNode(3)
+# root.right = TreeNode(4)
+# root.left.left = TreeNode(4)
+# root.left.right = TreeNode(2)
+# root.right.left = TreeNode(1)
+# root.right.right = TreeNode(5)
+# root.left.left.left = TreeNode(9)
+# root.left.left.right = TreeNode(10)
+# root.left.right.left = TreeNode(1)
+# root.left.right.right = TreeNode(4)
+# root.right.left.left = TreeNode(3)
+# root.right.left.right = TreeNode(5)
+# root.right.right.left = TreeNode(3)
+# root.right.right.right = TreeNode(5)
+cases.append(root)

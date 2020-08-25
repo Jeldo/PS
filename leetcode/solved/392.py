daad0f1fa8ba715299caf2db23562e998ee973f1
Jeldo <astrokwak@gmail.com>
2020-08-25 14:08:55 +0900

    392

---
+++ leetcode/solved/392.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str):
+        i = 0
+        if not s:
+            return True
+        for ch in t:
+            if ch == s[i]:
+                i += 1
+            if i == len(s):
+                return True
+        return False
+
+
+cases = [
+    ["abc",  "ahbgdc"],
+    ["axc", "ahbgdc"],
+    ["", "ahbgdc"],
+]
+
+for c in cases:
+    s = Solution().isSubsequence2(*c)
+    print(s)

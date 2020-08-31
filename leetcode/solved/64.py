52beaa98d0c6af970dd6a0fa088b80d4e327ecdb
Jeldo <astrokwak@gmail.com>
2020-08-31 15:49:25 +0900

    64

--- leetcode/solved/64.py
+++ leetcode/solved/64.py
@@ -7,31 +7,15 @@ Space Complexity: O(m*n)
 
 
 class Solution:
-    # use original grid
-    def minPathSum(self, grid):
-        for i in range(0, len(grid)):
-            for j in range(0, len(grid[0])):
-                if i == 0 and j > 0:
-                    grid[i][j] += grid[i][j-1]
-                elif j == 0 and i > 0:
-                    grid[i][j] += grid[i-1][j]
-                elif i > 0 and j > 0:
-                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+    def minPathSum(self, grid: list):
+        for i in range(1, len(grid)):
+            grid[i][0] += grid[i-1][0]
+        for j in range(1, len(grid[0])):
+            grid[0][j] += grid[0][j-1]
+        for i in range(1, len(grid)):
+            for j in range(1, len(grid[0])):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
         return grid[-1][-1]
-
-    # use new table to mark a cost
-    def minPathSum2(self, grid):
-        cost = [([0]*len(grid[0]))for _ in range(0, len(grid))]
-        cost[0][0] = grid[0][0]
-        for i in range(0, len(cost)):
-            for j in range(0, len(cost[0])):
-                if i == 0 and j > 0:
-                    cost[i][j] = grid[i][j] + cost[i][j-1]
-                elif j == 0 and i > 0:
-                    cost[i][j] = grid[i][j] + cost[i-1][j]
-                else:
-                    cost[i][j] = grid[i][j] + min(cost[i-1][j], cost[i][j-1])
-        return cost[-1][-1]
 
 
 cases = [
@@ -39,25 +23,6 @@ cases = [
         [1, 3, 1],
         [1, 5, 1],
         [4, 2, 1]
-    ],
-    [
-        [1, 2, 4, 1],
-        [1, 3, 1, 2],
-        [3, 1, 2, 1]
-    ],
-    [
-        [1]
-    ],
-    [
-        [1, 0],
-        [1, 1]
-    ],
-    [
-        [1],
-        [3]
-    ],
-    [
-        [1, 2]
     ]
 ]
 

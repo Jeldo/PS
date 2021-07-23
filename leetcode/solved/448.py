fe7b5a100b317d7d8900c7683c5ec31a2a19a071
Jeldo <astrokwak@gmail.com>
2021-07-24 00:03:14 +0900

    448

--- leetcode/solved/448.py
+++ leetcode/solved/448.py
@@ -1,11 +1,26 @@
 class Solution:
-    def findDisappearedNumbers(self, nums):
-        for i in range(0, len(nums)):
-            j = abs(nums[i])-1
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        for i in range(1, len(nums) + 1):
+            n = nums[i - 1]
+            while n != 0:
+                next_n, nums[n - 1] = nums[n - 1], 0
+                n = next_n
+
+        return [i + 1 for i in range(len(nums)) if nums[i] != 0]
+
+    def findDisappearedNumbers_abs(self, nums):
+        for i in range(len(nums)):
+            j = abs(nums[i]) - 1
             nums[j] = -abs(nums[j])
-        return [i+1 for i in range(0, len(nums)) if nums[i] > 0]
+        return [i + 1 for i in range(0, len(nums)) if nums[i] > 0]
 
 
-nums = [4, 3, 2, 7, 8, 2, 3, 1]
-s = Solution().findDisappearedNumbers(nums)
-print(s)
+cases = [
+    [4, 3, 2, 7, 8, 2, 3, 1],
+    [1, 1],
+    [1],
+]
+
+for c in cases:
+    res = Solution().findDisappearedNumbers(c)
+    print(res)

b110fb4e8e2dcacbca0c9803009e332dab0a761f
Jeldo <astrokwak@gmail.com>
2021-08-30 16:26:49 +0900

    63

--- leetcode/solved/63.py
+++ leetcode/solved/63.py
@@ -1,33 +1,33 @@
-'''
-Category: Dynamic Programming
-'''
-
-
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list):
-        grid = [[0]*len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        r, c = len(obstacleGrid), len(obstacleGrid[0])
+        if obstacleGrid[r - 1][c - 1] == 1:
+            return 0
+
+        dp = [[0] * c for _ in range(r)]
+        for i in range(r):
+            for j in range(c):
                 if obstacleGrid[i][j] == 1:
-                    grid[i][j] = -1
+                    dp[i][j] = -1
 
-        for i in range(len(grid)):
-            if grid[i][0] == -1:
+        for i in range(r):
+            if dp[i][0] == -1:
                 break
-            grid[i][0] = 1
+            dp[i][0] = 1
 
-        for j in range(len(grid[0])):
-            if grid[0][j] == -1:
+        for j in range(c):
+            if dp[0][j] == -1:
                 break
-            grid[0][j] = 1
+            dp[0][j] = 1
 
-        for i in range(1, len(grid)):
-            for j in range(1, len(grid[0])):
-                if grid[i][j] != -1:
-                    up = grid[i-1][j] if grid[i-1][j] != -1 else 0
-                    left = grid[i][j-1] if grid[i][j-1] != -1 else 0
-                    grid[i][j] = up + left
-        return grid[-1][-1] if grid[-1][-1] != -1 else 0
+        for i in range(1, r):
+            for j in range(1, c):
+                if dp[i][j] != -1:
+                    up = dp[i - 1][j] if dp[i - 1][j] != -1 else 0
+                    left = dp[i][j - 1] if dp[i][j - 1] != -1 else 0
+                    dp[i][j] = up + left
+
+        return dp[-1][-1]
 
 
 cases = [

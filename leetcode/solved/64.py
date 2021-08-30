dfbd7cd02a766cdc3858b0dc4c1c13665077ddb2
Jeldo <astrokwak@gmail.com>
2021-08-30 16:36:53 +0900

    64

--- leetcode/solved/64.py
+++ leetcode/solved/64.py
@@ -1,31 +1,35 @@
-'''
-Category: Dynamic Programming
-let (m, n) is the length of row and column of a grid.
-Time Complexity: O(m*n)
-Space Complexity: O(m*n)
-'''
+from typing import List
 
 
 class Solution:
     def minPathSum(self, grid: list):
         for i in range(1, len(grid)):
-            grid[i][0] += grid[i-1][0]
+            grid[i][0] += grid[i - 1][0]
         for j in range(1, len(grid[0])):
-            grid[0][j] += grid[0][j-1]
+            grid[0][j] += grid[0][j - 1]
         for i in range(1, len(grid)):
             for j in range(1, len(grid[0])):
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
+        return grid[-1][-1]
+
+    def minPathSum2(self, grid: List[List[int]]) -> int:
+        r, c = len(grid), len(grid[0])
+        for i in range(1, r):
+            grid[i][0] += grid[i - 1][0]
+        for i in range(1, c):
+            grid[0][i] += grid[0][i - 1]
+        for i in range(1, r):
+            for j in range(1, c):
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
         return grid[-1][-1]
 
 
 cases = [
-    [
-        [1, 3, 1],
-        [1, 5, 1],
-        [4, 2, 1]
-    ]
+    [[1, 3, 1], [1, 5, 1], [4, 2, 1]],
+    [[1, 2, 3], [4, 5, 6]]
 ]
 
 for c in cases:
-    s = Solution().minPathSum(c)
-    print(s)
+    print(Solution().minPathSum(c))

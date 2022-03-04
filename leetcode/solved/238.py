3e4f258fc3d5d5a546e1ff59d13dc82ae4cf05d7
Jeldo <astrokwak@gmail.com>
2022-03-04 20:10:44 +0900

    238

--- leetcode/solved/238.py
+++ leetcode/solved/238.py
@@ -1,37 +1,27 @@
-class Solution:
-    def productExceptSelf(self, nums: list[int]) -> list[int]:
-        result = []
-        p = 1
-        for i in range(len(nums)):
-            result.append(p)
-            p *= nums[i]
-        p = 1
-        for i in range(len(nums) - 1, -1, -1):
-            result[i] *= p
-            p *= nums[i]
-        return result
+from typing import List
 
-    def productExceptSelf2(self, nums):
-        result = [1] * len(nums)
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         left, right = 0, len(nums) - 1
-        left_product = right_product = 1
+        left_product, right_product = 1, 1
+        product = [1] * len(nums)
 
         while left < len(nums):
-            result[left] *= left_product
-            result[right] *= right_product
+            product[left] *= left_product
+            product[right] *= right_product
             left_product *= nums[left]
             right_product *= nums[right]
             left += 1
             right -= 1
 
-        return result
+        return product
 
 
 cases = [
     [1, 2, 3, 4],
-    [-1, 1, 0, -3, 3]
+    [-1, 1, 0, -3, 3],
 ]
 
 for c in cases:
-    res = Solution().productExceptSelf(c)
-    print(res)
+    print(Solution().productExceptSelf(c))

eed84d48d8671c52445dbbd5ba79840082f37df4
Jeldo <astrokwak@gmail.com>
2022-11-06 20:19:09 +0900

    890

--- leetcode/solved/890.py
+++ leetcode/solved/890.py
@@ -1,80 +1,30 @@
 '''
 Category: String
-Time Complexity: O(n)
+Time Complexity: O(N*K), N=the number of words, K=the length of each word
 '''
 
 
 class Solution:
-    def findAndReplacePattern(self, words: list, pattern: str):
-        pattern_dict = {}
-        i = 0
-        p = ''
-        answer = []
-        for ch in pattern:
-            if ch in pattern_dict.keys():
-                p += str(pattern_dict[ch])
-            else:
-                pattern_dict[ch] = i
-                p += str(i)
-                i += 1
-        for word in words:
-            p_dict = {}
-            w_i = 0
-            w_p = ''
-            for ch in word:
-                if ch in p_dict.keys():
-                    w_p += str(p_dict[ch])
-                else:
-                    p_dict[ch] = w_i
-                    w_p += str(w_i)
-                    w_i += 1
-            if w_p == p:
-                answer.append(word)
-        return answer
+    def findAndReplacePattern(self, words: list[str], pattern: str) -> list[str]:
+        def patternize(s):
+            pattern_dict = {}
+            p = 0
+            result = ''
+            for ch in s:
+                if ch not in pattern_dict:
+                    pattern_dict[ch] = str(p)
+                    p += 1
+                result += pattern_dict[ch]
+            return result
 
-    def findAndReplacePattern2(self, words, pattern):
-        answers = list()
-        pattern_dict = dict()
-        pattern_list = list()
-        cur_num = -1
-        cur_char = ''
-        for i in range(0, len(pattern)):
-            if cur_char == pattern[i]:
-                pattern_list.append(pattern_dict[cur_char])
-            else:
-                cur_num += 1
-                cur_char = pattern[i]
-                if cur_char not in pattern_dict:
-                    pattern_dict[cur_char] = cur_num
-                pattern_list.append(pattern_dict[cur_char])
-
-        for word in words:
-            is_valid = True
-            word_list = list()
-            word_dict = dict()
-            cur_word_num = -1
-            cur_word_char = ''
-            for i in range(0, len(word)):  # * n
-                if cur_word_char == word[i]:
-                    word_list.append(cur_word_num)
-                else:
-                    cur_word_num += 1
-                    cur_word_char = word[i]
-                    if cur_word_char not in word_dict:
-                        word_dict[cur_word_char] = cur_word_num
-                    if word_dict[cur_word_char] != pattern_list[i]:
-                        is_valid = False
-                        break
-                    word_list.append(word_dict[cur_word_char])
-            if is_valid and pattern_list == word_list:
-                answers.append(word)
-        return answers
+        pattern_digit = patternize(pattern)
+        return [word for word in words if pattern_digit == patternize(word)]
 
 
-words = ["qxrwtncxyoqwmsxoavos", "eqvzjrnqgkezxmqkhdkm",
-         "pjxmgdujohpmtsjhazhs", "yqhlipeqwnylkrqnsbnr", "plktdyslmoptqflowaof"]
-pattern = "ghtxpauhingxekhnoqnk"
-# words = ["badc","abab","dddd","dede","yyxx"]
-# pattern = "baba"
-s = Solution().findAndReplacePattern(words, pattern)
-print(s)
+cases = [
+    [["abc", "deq", "mee", "aqq", "dkd", "ccc"], "abb"],
+    [["a", "b", "c"], "a"],
+]
+
+for c in cases:
+    print(Solution().findAndReplacePattern(*c))

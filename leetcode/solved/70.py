b0285dbf0acf0fd11021a6cbe847ea578eed49c4
jeldo <astrokwak@gmail.com>
2024-01-19 12:22:47 +0900

    70

--- leetcode/solved/70.py
+++ leetcode/solved/70.py
@@ -1,30 +1,10 @@
-'''
-Category: Dynamic Programming
-Time Complexity: O(n)
-Space Complexity: O(n) or O(1)
-'''
-
-
+# O(n)
 class Solution:
-    def climbStairs(self, n: int):
-        cost = [1, 2] + [0] * (n - 2)
-        if n == 1:
-            return cost[0]
-        elif n == 2:
-            return cost[1]
+    def climbStairs(self, n: int) -> int:
+        steps = [1, 2]
         for i in range(2, n):
-            cost[i] = cost[i-1] + cost[i-2]
-        return cost[n-1]
-
-    def climbStairs2(self, n: int):
-        if n == 1:
-            return 1
-        elif n == 2:
-            return 2
-        steps_1, steps_2 = 1, 2
-        for i in range(3, n + 1):
-            steps_1, steps_2 = steps_2, steps_1 + steps_2
-        return steps_2
+            steps.append(steps[i - 2] + steps[i - 1])
+        return steps[n-1]
 
 
 cases = [
@@ -32,5 +12,4 @@ cases = [
 ]
 
 for c in cases:
-    s = Solution().climbStairs2(c)
-    print(s)
+    print(Solution().climbStairs(c))

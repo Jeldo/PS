8d71150a04c555db5091a0a4969a47bcafc6c4ab
jeldo <astrokwak@gmail.com>
2024-01-21 17:42:29 +0900

    198

--- leetcode/solved/198.py
+++ leetcode/solved/198.py
@@ -1,13 +1,24 @@
+# O(n)
+from typing import List
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        before_3, before_2 = 0, 0
-        max_num = max(nums[:3])
-        for i in range(2, len(nums)):
-            if 0 <= i - 2:
-                before_2 = nums[i - 2]
-            if 0 <= i - 3:
-                before_3 = nums[i - 3]
-            nums[i] += max(before_2, before_3)
-            max_num = max(max_num, nums[i])
+        max_rob = 0
+        for i in range(len(nums)):
+            if 3 <= i:
+                nums[i] += max(nums[i-3], nums[i-2])
+            elif 2 <= i:
+                nums[i] += nums[i-2]
+            max_rob = max(max_rob, nums[i])
 
-        return max_num
+        return max_rob
+
+
+cases = [
+    [1, 2, 3, 1],
+    [2, 7, 9, 3, 1],
+]
+
+for c in cases:
+    print(Solution().rob(c))

aae1e72c73cbb36d7797819994310b0e120e10b8
jeldo <astrokwak@gmail.com>
2024-03-27 22:51:22 +0900

    56

--- leetcode/solved/56.py
+++ leetcode/solved/56.py
@@ -1,39 +1,13 @@
-from collections import deque
-
-
 class Solution:
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
-        result = []
         intervals.sort()
+        merged = []
         for interval in intervals:
-            if not result or result[-1][1] < interval[0]:
-                result.append(interval)
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
             else:
-                result[-1][1] = max(result[-1][1], interval[1])
-
-        return result
-
-    def merge2(self, intervals: list[list[int]]) -> list[list[int]]:
-        intervals.sort()
-        intervals = deque(intervals)
-        start, end = intervals[0][0], intervals[0][1]
-        result = []
-
-        while intervals:
-            if intervals[0][0] <= end:
-                _, e = intervals.popleft()
-                end = max(end, e)
-            if not intervals:
-                break
-            if end < intervals[0][1]:
-                result.append([start, end])
-                start, end = intervals[0][0], intervals[0][1]
-        if not result:
-            result.append([start, end])
-        if result[-1] != [start, end]:
-            result.append([start, end])
-
-        return result
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
 
 
 cases = [

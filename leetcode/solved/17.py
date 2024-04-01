c4a48fce706024d07ab9183907609aecb1397a2f
jeldo <astrokwak@gmail.com>
2024-04-01 22:43:51 +0900

    17

--- leetcode/solved/17.py
+++ leetcode/solved/17.py
@@ -1,28 +1,34 @@
 class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
-        nums = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'],
-                '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
+        num_to_letter = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
 
-        def dfs(combination, d):
-            if not d:
-                if len(combination) > 0:
-                    combinations.append(combination)
+        result = []
+
+        def dfs(letters, digit):
+            if not digit:
+                if letters:
+                    result.append(letters)
                 return
-            for ch in nums[d[0]]:
-                dfs(combination + ch, d[1:])
-
-        combinations = []
-        dfs('', digits)
-
-        return combinations
+            for ch in num_to_letter[digit[0]]:
+                dfs(letters + ch, digit[1:])
+        dfs("", digits)
+        return result
 
 
 cases = [
-    '23',
-    '',
-    '2',
+    "23",
+    "",
+    "2",
 ]
 
 for c in cases:
-    res = Solution().letterCombinations(c)
-    print(res)
+    print(Solution().letterCombinations(c))

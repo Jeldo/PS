2b1b057adb53391b4f0a27e7c4a2e2ff1679c501
jeldo <astrokwak@gmail.com>
2024-05-26 18:36:43 +0900

    392

--- leetcode/solved/392.py
+++ leetcode/solved/392.py
@@ -1,14 +1,11 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str):
-        i = 0
-        if not s:
-            return True
-        for ch in t:
-            if ch == s[i]:
-                i += 1
-            if i == len(s):
-                return True
-        return False
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_i, t_i = 0, 0
+        while s_i < len(s) and t_i < len(t):
+            if s[s_i] == t[t_i]:
+                s_i += 1
+            t_i += 1
+        return s_i == len
 
 
 cases = [
@@ -18,5 +15,4 @@ cases = [
 ]
 
 for c in cases:
-    s = Solution().isSubsequence(*c)
-    print(s)
+    print(Solution().isSubsequence(*c))

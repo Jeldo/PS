562a7720ec324d51285f3c609e217ef5712341b9
jeldo <astrokwak@gmail.com>
2025-01-25 21:43:33 +0900

    73

--- leetcode/solved/73.py
+++ leetcode/solved/73.py
@@ -1,18 +1,22 @@
 class Solution:
+    # Time: O(m*n), m = len(matrix), len(matrix[0])
+    # Space: O(m+n)
     def setZeroes(self, matrix: list[list[int]]) -> None:
-        def set_zeros(r, c):
-            for i in range(len(matrix)):
-                matrix[i][c] = 0
-            for i in range(len(matrix[0])):
-                matrix[r][i] = 0
-
-        zeros = []
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row_zeros, col_zeros = set(), set()
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    zeros.append((i, j))
-        for r, c in zeros:
-            set_zeros(r, c)
+                    row_zeros.add(i)
+                    col_zeros.add(j)
+        for r in row_zeros:
+            for j in range(len(matrix[0])):
+                matrix[r][j] = 0
+        for c in col_zeros:
+            for i in range(len(matrix)):
+                matrix[i][c] = 0
 
 
 cases = [
@@ -21,5 +25,4 @@ cases = [
 ]
 
 for c in cases:
-    res = Solution().setZeroes(c)
-    print(res)
+    print(Solution().setZeroes(c))
